08476c4b4faecb71a2dc6f49037dce5a503d1087
LostNeophyte <lostneophyte@tuta.io>
2022-11-10 15:53:22 +0100

    first working version, configurable rules, comments

--- lua/bigfile/features.lua
+++ lua/bigfile/features.lua
@@ -1,6 +1,27 @@
 local M = {}
 
-M["match_paren"] = {
+---@class feature
+---@field disable function|nil Disables the feature
+---@field enable function|nil Enables the feature
+---@field global boolean|nil If true the feature has a global effect
+---@field defer boolean|nil If true the feature will be disabled in vim.schedule
+---@field [1] string Name of the feature
+
+---@return feature
+function M.get_feature(raw_feature)
+  if (type(raw_feature) == "string") then -- builtin feature
+    return M[raw_feature];
+  else -- custom feature
+    return raw_feature
+  end
+end
+
+local function feature(name, content)
+  M[name] = content
+  M[name][1] = name
+end
+
+feature("match_paren", {
   global = true,
   disable = function()
     vim.cmd "NoMatchParen"
@@ -8,10 +29,6 @@ M["match_paren"] = {
   enable = function()
     vim.cmd "DoMatchParen"
   end
-}
-
-for name, _ in pairs(M) do
-  M[name][1] = name
-end
+})
 
 return M
--- lua/bigfile/init.lua
+++ lua/bigfile/init.lua
@@ -2,25 +2,28 @@ local M = {}
 
 local features = require("bigfile.features")
 
----@type table[]
+-- list of open big buffers
+---@type {disabled_global_features: feature[], all_disabled_features: feature[]}[]
 local big_buffers = {}
 
 ---@class rule
 ---@field size number file size in MiB
----@field features (string|table)[] feature name or a custom feature
+---@field features (string|table)[] array of features
 
 ---@class config
 ---@field rules rule[] rules
 local config = {
   rules = {
-    { size = 0.001, features = { "match_paren", { "nvim_navic" }, { "lsp" } } },
+    { size = 0.0000, features = { "match_paren", { "nvim_navic" }, { "lsp" } } },
   }
 }
 
--- checks if feature is disabled in `bufnr` buffer
+---@param bufnr number
+---@param feature_name string
+---@return boolean is_disabled Ture if `feature_name` is disabled in `bufnr` buffer
 function M.is_feature_disabled(bufnr, feature_name)
   if big_buffers[bufnr] ~= nil then
-    local disabled_features = big_buffers[bufnr].disabled_features
+    local disabled_features = big_buffers[bufnr].all_disabled_features
     for _, feature in ipairs(disabled_features) do
       if feature[1] == feature_name then
         return true
@@ -30,7 +33,8 @@ function M.is_feature_disabled(bufnr, feature_name)
   return false
 end
 
--- returns all features from config.rules that match the `filesize`
+---@param filesize number File size in MiB
+---@return feature[] features Features from rules that match the `filesize`
 local function match_features(filesize)
   local MB = 1024 * 1024
   local matched_features = {}
@@ -38,33 +42,36 @@ local function match_features(filesize)
     if filesize >= rule.size * MB then
 
       for _, raw_feature in ipairs(rule.features) do
-        local feature
-        if (type(raw_feature) == "string") then
-          feature = features[raw_feature];
-        else
-          feature = raw_feature
-        end
-        table.insert(matched_features, feature)
+        table.insert(matched_features, features.get_feature(raw_feature))
       end
 
-    else
+    else -- since rules should be sorted, we can exit early
       return matched_features
     end
   end
   return matched_features
 end
 
+-- Enables global features that aren't disabled by different buffers
 local function enable_global_features(features_to_enable)
-  -- TODO: don't enable features present in big_buffers
+  local features_not_to_touch = {}
+  for _, big_buffer in pairs(big_buffers) do
+    vim.list_extend(features_not_to_touch, big_buffer.disabled_global_features[1])
+  end
+  vim.pretty_print(features_not_to_touch)
+  vim.pretty_print(big_buffers)
+
   for _, feature in ipairs(features_to_enable) do
-    feature.enable()
+    if not vim.tbl_contains(features_not_to_touch, feature[1]) then
+      feature.enable()
+    end
   end
 end
 
--- main function of the plugin
+-- disables features matching the size of the `args.buf` buffer
 local function pre_bufread_callback(args)
   if big_buffers[args.buf] ~= nil then
-    return
+    return -- buffer aleady set-up
   end
 
   local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(args.buf))
@@ -76,12 +83,12 @@ local function pre_bufread_callback(args)
   if #matched_features == 0 then
     return
   end
+  print(args.buf)
 
+  -- Categorize features and disable features that don't need deferring
   local matched_global_features = {}
   local matched_deferred_features = {}
-
   for _, feature in ipairs(matched_features) do
-
     if feature.global then
       table.insert(matched_global_features, feature)
     end
@@ -95,12 +102,14 @@ local function pre_bufread_callback(args)
 
   big_buffers[args.buf] = {
     disabled_global_features = matched_global_features,
-    disabled_features = matched_features
+    all_disabled_features = matched_features
   }
 
+  -- Setup an autocommand to enable features after the bugger is deleted
   if #matched_global_features > 0 then
     vim.api.nvim_create_autocmd({ "BufDelete" }, {
       callback = function()
+        print("deleting ", args.buf)
         local features_to_enable = big_buffers[args.buf].disabled_global_features
         big_buffers[args.buf] = nil
         enable_global_features(features_to_enable)
@@ -109,6 +118,7 @@ local function pre_bufread_callback(args)
     })
   end
 
+  -- Schedule disabling deferred features
   vim.schedule(function()
     vim.api.nvim_buf_call(args.buf, function()
       for _, feature in ipairs(matched_deferred_features) do
@@ -119,14 +129,19 @@ local function pre_bufread_callback(args)
 
 end
 
-local function setup_autocmd()
+---@param user_config config|nil
+function M.setup(user_config)
+  if type(user_config) == "table" then
+    if user_config.rules then
+      config.rules = user_config.rules
+    end
+  end
+
   vim.api.nvim_create_augroup("bigfile", {})
   vim.api.nvim_create_autocmd("BufReadPre", {
     group = "bigfile",
     callback = pre_bufread_callback
   })
 end
-
-setup_autocmd()
 
 return M;
