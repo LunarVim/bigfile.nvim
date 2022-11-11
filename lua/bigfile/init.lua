e2fa8b2726c87d7fc46a40d50e89e97d90de05fa
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-11-11 15:08:59 +0100

    refactor: use a single bufvar for tracking files

--- lua/bigfile/init.lua
+++ lua/bigfile/init.lua
@@ -2,13 +2,6 @@ local M = {}
 
 local features = require("bigfile.features")
 
----@class big_buffer
----@field all_disabled_features feature[]
----@field disabled_global_features feature[]
--- list of open big buffers
----@type big_buffer[]
-local big_buffers = {}
-
 ---@class rule
 ---@field size integer file size in MiB
 ---@field features feature[] array of features
@@ -20,9 +13,9 @@ local config = {
     {
       size = 1,
       features = {
-        "indent_blankline", "illuminate", { "nvim_navic" },
+         "vimopts", "indent_blankline", "illuminate", { "nvim_navic" },
         "treesitter", "syntax",
-        "matchparen", "swapfile", "undofile",
+        "matchparen",
       }
     },
     { size = 2, features = { { "lsp" } } },
@@ -31,28 +24,17 @@ local config = {
 }
 
 ---@param bufnr number
----@param feature_name string
----@return boolean is_disabled Ture if `feature_name` is disabled in `bufnr` buffer
-function M.is_feature_disabled(bufnr, feature_name)
-  if big_buffers[bufnr] ~= nil then
-    local disabled_features = big_buffers[bufnr].all_disabled_features
-    for _, feature in ipairs(disabled_features) do
-      if feature[1] == feature_name then
-        return true
-      end
-    end
-  end
-  return false
-end
-
----@param bufnr number
 ---@return integer|nil size in MiB if buffer is valid, nil otherwise
 local function get_buf_size(bufnr)
-  local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
+
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+  local ok, stats = pcall(function()
+    return vim.loop.fs_stat(vim.api.nvim_buf_get_name(bufnr))
+  end)
   if not (ok and stats) then
     return
   end
-  return stats.size * 1024 * 1024
+  return math.floor(0.5 + (stats.size / (1024 * 1024)))
 end
 
 ---@param bufnr number buffer id to match against
@@ -65,11 +47,9 @@ local function match_features(bufnr)
   end
   for _, rule in ipairs(config.rules) do
     if filesize >= rule.size then
-
       for _, raw_feature in ipairs(rule.features) do
         table.insert(matched_features, features[raw_feature])
       end
-
     else -- since rules should be sorted, we can exit early
       return matched_features
     end
@@ -77,65 +57,29 @@ local function match_features(bufnr)
   return matched_features
 end
 
--- Enables global features that aren't disabled by different buffers
-local function enable_global_features(buf, features_to_enable)
-  local features_not_to_touch = {}
-  for _, big_buffer in pairs(big_buffers) do
-    for _, global_feature in pairs(big_buffer.disabled_global_features) do
-      table.insert(features_not_to_touch, global_feature[1])
-    end
-  end
-
-  for _, feature in ipairs(features_to_enable) do
-    if not vim.tbl_contains(features_not_to_touch, feature[1]) then
-      if type(feature.enable) == "function" then
-        feature.enable(buf)
-      end
-    end
-  end
-end
-
 -- disables features matching the size of the `args.buf` buffer
 local function pre_bufread_callback(args)
-  if big_buffers[args.buf] ~= nil then
-    return -- buffer aleady set-up
+  local status_ok, _ = pcall(vim.api.nvim_buf_get_var, args.buf, "bigfile_detected")
+  if status_ok then
+    return -- buffer has already been processed
   end
 
   local matched_features = match_features(args.bufnr)
   if #matched_features == 0 then
+    vim.api.nvim_buf_set_var(args.buf, "bigfile_detected", 0)
     return
   end
 
+  vim.api.nvim_buf_set_var(args.buf, "bigfile_detected", 1)
+
   -- Categorize features and disable features that don't need deferring
-  local matched_global_features = {}
   local matched_deferred_features = {}
   for _, feature in ipairs(matched_features) do
-    if feature.opts.global then
-      table.insert(matched_global_features, feature)
-    end
-
     if feature.opts.defer then
       table.insert(matched_deferred_features, feature)
     else
       feature.disable(args.buf)
     end
-  end
-
-  big_buffers[args.buf] = {
-    disabled_global_features = matched_global_features,
-    all_disabled_features = matched_features
-  }
-
-  -- Setup an autocommand to enable features after the bugger is deleted
-  if #matched_global_features > 0 then
-    vim.api.nvim_create_autocmd({ "BufDelete" }, {
-      callback = function()
-        local features_to_enable = big_buffers[args.buf].disabled_global_features
-        big_buffers[args.buf] = nil
-        enable_global_features(args.buf, features_to_enable)
-      end,
-      buffer = args.buf,
-    })
   end
 
   -- Schedule disabling deferred features
@@ -146,7 +90,6 @@ local function pre_bufread_callback(args)
       end
     end)
   end)
-
 end
 
 ---@param user_config config|nil
@@ -158,10 +101,10 @@ function M.setup(user_config)
   end
 
   vim.api.nvim_create_augroup("bigfile", {})
-  vim.api.nvim_create_autocmd("BufReadPre", {
+  vim.api.nvim_create_autocmd("BufReadPost", {
     group = "bigfile",
     callback = pre_bufread_callback
   })
 end
 
-return M;
+return M
