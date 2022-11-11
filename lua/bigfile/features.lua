7bd57931ab59046f0e25ff881f619a993b54fd80
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-11-11 10:41:09 +0100

    refactor: more flexible features

--- lua/bigfile/features.lua
+++ lua/bigfile/features.lua
@@ -1,86 +1,77 @@
 local M = {}
 
----@class feature
----@field disable function|nil Disables the feature
----@field enable function|nil Enables the feature
+---@class featureOpts
 ---@field global boolean|nil If true the feature has a global effect
 ---@field window_local boolean|nil If true the feature has a window local effect
 ---@field defer boolean|nil If true the feature will be disabled in vim.schedule
----@field [1] string Name of the feature
 
----@return feature
-function M.get_feature(raw_feature)
-  if (type(raw_feature) == "string") then -- builtin feature
-    if not M[raw_feature] then
-      vim.notify("bigfile.nvim: feature " .. raw_feature .. " does not exist!", vim.log.levels.WARN)
-    end
-    return M[raw_feature];
-  else -- custom feature
-    return raw_feature
-  end
-end
+---@class feature
+---@field disable function Disables the feature
+---@field enable function|nil Enables the feature
+---@field opts featureOpts
 
--- TODO: find out why these couse an error in cmp when inside `content` of function call
---[[ ---@param name string
----@param content feature ]]
+---Add feature
+---@param name string
+---@param content feature
 local function feature(name, content)
+  vim.validate {
+    name = { name, "string" },
+    content = { content, "table" },
+    disable = { content.disable, "function" },
+    enable = { content.enable, "function", true },
+    opts = { content.opts, "table", true },
+  }
+  content.opts = content.opts or {}
   M[name] = content
-  M[name][1] = name
+  M[name].name = name
 end
-
-local function local_feature(name, option, value)
-  option = option or name
-  value = value or false
-  feature(name, {
-    disable = function()
-      vim.opt_local[option] = value
-    end,
-  })
-end
-
-local local_features = { "swapfile", "undofile", "list" }
-
-for _, feat in pairs(local_features) do
-  local_feature(feat)
-end
-
 
 feature("matchparen", {
-  global = true,
+  opts = { global = true },
   disable = function()
+    if vim.fn.exists ":DoMatchParen" ~= 2 then
+      return
+    end
     vim.cmd "NoMatchParen"
   end,
   enable = function()
     vim.cmd "DoMatchParen"
-  end
+  end,
 })
+
+local function nvim_set_local(option, value)
+  vim.api.nvim_set_option_value(option, value { scope = "local" })
+end
 
 feature("syntax", {
-  global = true,
   disable = function()
-    vim.cmd "syntax off"
+    if not vim.g.syntax_on then
+      return
+    end
+    nvim_set_local("syntax", "OFF")
     vim.cmd "syntax clear"
   end,
-  enable = function()
-    vim.cmd "syntax on"
-  end
 })
 
-feature("filetype", {
-  global = true,
+feature("vimopts", {
   disable = function()
-    vim.cmd "filetype off"
+    nvim_set_local("filetype", "")
+    nvim_set_local("swapfile", false)
+    nvim_set_local("list", false)
+    nvim_set_local("foldmethod", "manual")
+    nvim_set_local("undolevels", -1)
+    nvim_set_local("undoreload", 0)
   end,
-  enable = function()
-    vim.cmd "filetype on"
-  end
 })
 
 feature("treesitter", {
-  defer = true,
+  opts = { defer = true },
   disable = function()
-    vim.cmd "TSBufDisable highlight"
-    vim.cmd "TSBufDisable indent"
+    pcall(function()
+      local ts_conf = require "nvim-treesitter.configs"
+      local available_modules = ts_conf.available_modules()
+      vim.cmd("TSBufDisable " .. unpack(available_modules))
+    end)
   end,
 })
 
@@ -99,5 +90,17 @@ feature("indent_blankline", {
     end)
   end,
 })
+
+----@return feature
+function M.get_feature(raw_feature)
+  if type(raw_feature) == "string" then -- builtin feature
+    if not M[raw_feature] then
+      vim.notify("bigfile.nvim: feature " .. raw_feature .. " does not exist!", vim.log.levels.WARN)
+    end
+    return M[raw_feature]
+  else -- custom feature
+    return raw_feature
+  end
+end
 
 return M
